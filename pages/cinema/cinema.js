f301f316d3bafe85968f84800f633dfca7b8254f
yankun11 <k1833479910@163.com>
2022-08-17 23:09:15 +0800

    频道页的顶部滚动导航栏

--- pages/cinema/cinema.js
+++ pages/cinema/cinema.js
@@ -1,66 +1,21 @@
 // pages/cinema/cinema.js
 Page({
-
-	/**
-	 * 页面的初始数据
-	 */
 	data: {
-
+		scrollViewTitle:[
+			'Vip专享','视频','图片','趣事','精华','穿越','爆料'
+		],
+		currentIndex:0,
+		winHeight:0,
+		vipPages:1,
+		vipArray:[]
+	},
+	changeCurrentIndex(e){
+		this.setData({
+			currentIndex:e.currentTarget.id
+		})
 	},
 
-	/**
-	 * 生命周期函数--监听页面加载
-	 */
 	onLoad(options) {
 
 	},
-
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady() {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow() {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide() {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload() {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh() {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom() {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage() {
-
-	}
 })
