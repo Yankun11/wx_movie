67e89e56317cbc678b5cdcb23966ac8f07a7daa4
yankun11 <k1833479910@163.com>
2022-08-18 21:00:06 +0800

    vip专享内容

--- pages/cinema/cinema.js
+++ pages/cinema/cinema.js
@@ -5,7 +5,7 @@ Page({
 			'Vip专享','视频','图片','趣事','精华','穿越','爆料'
 		],
 		currentIndex:0,
-		winHeight:0,
+		winHeight:110,
 		vipPages:1,
 		vipArray:[]
 	},
@@ -14,8 +14,32 @@ Page({
 			currentIndex:e.currentTarget.id
 		})
 	},
-
+// 请求数据
 	onLoad(options) {
-
+		// 第一次加载时，加载第一页
+		this.loadVip(this.data.vipPages);
 	},
+	// 加载vip内容
+	loadVip(pages){
+		var url = "http://m2.qiushibaike.com/article/list/text?type=refresh&count=12&page=" + pages;
+		wx.request({
+			url: url,
+			success:res=>{
+				var vipArr = [...this.data.vipArray,...res.data.items]
+				this.setData({
+					vipArray:vipArr
+				});
+				wx.getSystemInfo({
+					success: (result) => {
+						this.setData({
+							winHeight:result.windowHeight * vipArr.length / 2
+						})
+					},
+				});
+				this.setData({
+					vipPages:pages
+				})
+			}
+		});
+	}
 })
